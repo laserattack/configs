bbbba1b015a2353fc5ca3b31a5d1eebadb26851c
serr <sc7227484@gmail.com>
2025-08-17 18:49:49 +0300

    some changes

--- gih.sh
+++ gih.sh
@@ -13,19 +13,18 @@ COPIED_COUNT=0
 
 for item in "${ITEMS_TO_COPY[@]}"; do
     if [ -e "$item" ]; then
-        dest_path="$DEST_DIR/$(basename "$item")"
-        
+        safe_name="${item//\//_}"
+        dest_path="$DEST_DIR/$safe_name"        
         if [ -e "$dest_path" ]; then
             echo "Removing existing: $dest_path"
             rm -rf "$dest_path"
         fi
         
         echo "Copying: $item → $dest_path"
-        cp -r "$item" "$DEST_DIR"/
+        cp -r "$item" "$dest_path"
         ((COPIED_COUNT++))
     else
         echo "Error: '$item' does not exist, skipping"
     fi
 done
 
-echo "Done! Copied $COPIED_COUNT/${#ITEMS_TO_COPY[@]} objects"
